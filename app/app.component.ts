32794c2dcec2c660117aa59725b3970375c513c2
ShdwBt <ftwfamily@gmail.com>
2016-09-15 16:45:41 +0200

    Player editor done

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,28 @@
 import { Component } from '@angular/core';
 
+export class Player {
+    id: number;
+    name: string;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template:`
+        <h1>{{title}}</h1>
+        <h2>{{player.name}} details!</h2>
+        <div><label>id: </label>{{player.id}}</div>
+        <div>
+            <label>name: </label>
+            <input [(ngModel)]="player.name" placeholder="name">
+        </div>
+    `
 })
-export class AppComponent { }
+
+export class AppComponent {
+    title = 'Tour of Players';
+    player: Player = {
+        id: 1,
+        name: 'Zlatan'
+    };
+}
+
